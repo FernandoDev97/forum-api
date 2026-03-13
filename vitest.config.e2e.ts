5414bf59672e5e2ed6478292c69f842a5758ccdb
Fernando Souza <l.fernando.dev@gmail.com>
2026-03-13 18:26:45 -0300

    feat: add config to vitest

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,20 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  esbuild: false,
+  oxc: false,
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
